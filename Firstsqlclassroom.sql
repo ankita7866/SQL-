1b8dcb1eaf5c195d19c1354799b2fd12f9fcfc35
Ankita <indulkar1995@gmail.com>
2026-07-29 09:03:12 +0530

    New file

---
+++ Firstsqlclassroom.sql
@@ -0,0 +1,12 @@
+show databases ;
+create database BankingDB_T385;
+use BankingDB_T385;
+show tables;
+CREATE TABLE Customers
+(
+CustomerID INT,
+FirstName VARCHAR(50),
+LastName VARCHAR(50),
+Email VARCHAR(100),
+Phone VARCHAr(15)
+);
